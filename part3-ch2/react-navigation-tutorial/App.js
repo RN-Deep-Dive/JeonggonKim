a4c67892da70fdc72cee20fc957b56ce99cbd900
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-10-07 02:35:10 +0900

    part3-ch2-4

--- part3-ch2/react-navigation-tutorial/App.js
+++ part3-ch2/react-navigation-tutorial/App.js
@@ -4,18 +4,42 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ScreenA } from './src/ScreenA';
 import { ScreenB } from './src/ScreenB';
 import { NestedStackNavigation } from './src/NestedStackNavigator';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { TabA } from './src/TabA';
+import { TabB } from './src/TabB';
+import { Ionicons } from '@expo/vector-icons';
+import { BottomTabNavigation } from './src/BottomTabNavigator';
 
 const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
 
-export default function App() {
+
+// STACK NAVIGATOR 
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name='ScreenA' component={ScreenA}/>
+//         <Stack.Screen name='ScreenB' component={ScreenB}/>
+//         <Stack.Screen name='NestedNavigator' component={NestedStackNavigation}/>
+
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+
+export default function App(){
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='ScreenA' component={ScreenA}/>
-        <Stack.Screen name='ScreenB' component={ScreenB}/>
-        <Stack.Screen name='NestedNavigator' component={NestedStackNavigation}/>
-
+        <Stack.Screen name={'NesteBottomTab'} component={BottomTabNavigation}/>
+        <Stack.Screen name={'ScreenB'} component={ScreenB}/>
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
+
+<BottomTab.Navigator>
+
+      </BottomTab.Navigator>
--- part3-ch2/react-navigation-tutorial/src/NestedStackNavigator.js
+++ part3-ch2/react-navigation-tutorial/src/NestedStackNavigator.js
@@ -2,6 +2,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, { Component } from 'react';
 import {View,Text, Button} from 'react-native';
 import {ScreenC} from './ScreenC';
+import { ScreenA } from './ScreenA';
+import { ScreenB } from './ScreenB';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,28 +11,11 @@ export class NestedStackNavigation extends Component{
   render(){
     return (
       <Stack.Navigator>
+        <Stack.Screen name="ScreenA" component={ScreenA}/>
+        {/* <Stack.Screen name="ScreenB" component={ScreenB}/> */}
         <Stack.Screen name="ScreenC" component={ScreenC}/>
       </Stack.Navigator>
     )
   }
 }
 
-
-
-
-export class ScreenA extends React.Component {
-  render(){
-    return (
-      <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-        <Text>ScreenA</Text>
-        <Button
-          title='B스크린으로 이동하기'
-          onPress={()=> {
-            this.props.navigation.navigate('ScreenB', {value:'fromA'});
-          }} 
-        />
-      </View>
-
-    )
-  }
-}
